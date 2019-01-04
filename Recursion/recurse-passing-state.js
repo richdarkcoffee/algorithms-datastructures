5afed8a59a2d5e344aa29b587e6e41556b8c9cde
Richard Coffey <richardc@no61rustandblue.lan>
2019-01-03 16:48:07 -0800

    Updated quicksort

--- Recursion/recurse-passing-state.js
+++ Recursion/recurse-passing-state.js
@@ -1,24 +1,37 @@
 function quicksortBasic(array) {
     if (array.length < 2) {
+        console.log(`Length: ${array.length}`);
         return array;
+    } else if (array.length == 2) {
+        if (array[0] < array [1]) return array;
+        else {
+            tmp  = array[0];
+            array[0] = array[1];
+            array[1] = tmp;
+            console.log('Swapped array elements.')
+            return array;
+        }
     } else {
         lesspivot = [];
         greaterpivot = [];
         finalarray = [];
-        pivot = array[array.length/2];
-        console.log(pivot);
+        pivot = array[1];
+        console.log(`Length: ${array.length}`);
 
         array.forEach(element => {
             if (element < pivot) lesspivot.push(element);
             if (element > pivot) greaterpivot.push(element);
         });
-        finalarray = (quicksortBasic(lesspivot).concat([pivot])).concat(quicksortBasic(greaterpivot));
-        console.log(finalarray);
+        console.log(`LESS: ${lesspivot} PIVOT: ${pivot} - GREATER: ${greaterpivot}`)
+        if (greaterpivot.length > 0) var returngreater = quicksortBasic(greaterpivot);
+        if (lesspivot.length > 0 ) var returnless = quicksortBasic(lesspivot);
+        var finalarray = [].concat(returnless,pivot,returngreater);
+        console.log(`${finalarray}`);
         return finalarray;
     }
 }
 
-sortme = [4,5,2,10,12,3,4,7,9,13];
+sortme = [5,20,4,2,10,3,12,1,7,9,13];
 
 console.log(sortme);
 console.log(quicksortBasic(sortme));
